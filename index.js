7d3cd2307911e37d76e185e78f570283802f3a9b
user <user@163.com>
2016-12-12 15:36:38 +0800

    提交

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('1 主分支 - 添加当前代码');
-
+console.log('修改内容';)
 // 这是在dev分支上修改的内容！
 var num = 123;
 console.log(num);
@@ -8,3 +8,17 @@ console.log(num);
 var say = function() {
   console.log('Hello github!!!');
 };
+B
+:wq
+
+
+
+
+
+
+:wq
+:w
+w
+:wq
+
+
